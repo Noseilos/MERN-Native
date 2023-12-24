8cfdc8a2d12040e64bfe8515fc699b810f8adcad
Noseilos <jonaseguiron@gmail.com>
2023-12-24 23:28:45 +0800

    create new product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Product } = require('../models/Product');
+const { Category } = require('../models/Category');
 
 router.get(`/`, async (req, res) => {
     const products = await Product.find()
@@ -12,21 +13,33 @@ router.get(`/`, async (req, res) => {
     res.send(products);
 })
 
-router.post(`/`, (req, res) => {
-    const newProduct = new Product({
+router.post(`/`, async (req, res) => {
+    const category = await Category.findById(req.body.category);
+    if (!category) {
+        return res.status(400).send(`Invalid Category`);
+    }
+
+    const product = new Product({
         name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
+        description: req.body.description,
+        longDescription: req.body.longDescription,
+        images: req.body.images,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
     });
 
-    newProduct.save().then((createdProduct => {
-        res.status(201).json(createdProduct);
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
+    const newProduct = await product.save();
+
+    if (!newProduct) {
+        return res.status(500).send(`Product can't be created`)
+    }
+
+    return res.send(newProduct);
 })
 
 module.exports = router;
