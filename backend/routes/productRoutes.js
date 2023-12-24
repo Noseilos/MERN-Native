a99738cf10fe44bef297c8d33f3829e76f9b7dec
Noseilos <jonaseguiron@gmail.com>
2023-12-25 06:56:15 +0800

    delete product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { Product } = require('../models/Product');
 const { Category } = require('../models/Category');
+const mongoose = require('mongoose');
 
 router.get(`/`, async (req, res) => {
     const products = await Product.find().populate('category');
@@ -54,6 +55,10 @@ router.get(`/:id`, async (req, res) => {
 })
 
 router.put(`/:id`, async (req, res) => {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(400).send(`Invalid Product Id`);
+    }
+
     const category = await Category.findById(req.body.category);
     if (!category) {
         return res.status(400).send(`Invalid Category`);
@@ -83,6 +88,18 @@ router.put(`/:id`, async (req, res) => {
     }
 
     res.send(product);
+})
+
+router.delete('/:id', (req, res) => {
+    Product.findByIdAndDelete(req.params.id).then(product => {
+        if (product) {
+            return res.status(200).json({success: true, message: 'Product deleted!'})
+        } else {
+            return res.status(404).json({success: false, message: 'Product not found'})
+        }
+    }).catch(err => {
+        return res.status(400).json({success: false, error: err})
+    });
 })
 
 module.exports = router;
