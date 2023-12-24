cce210b61e462ca3a6cfb4824dcf191b6b3fef78
Noseilos <jonaseguiron@gmail.com>
2023-12-25 06:47:35 +0800

    update product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -53,4 +53,36 @@ router.get(`/:id`, async (req, res) => {
 
 })
 
+router.put(`/:id`, async (req, res) => {
+    const category = await Category.findById(req.body.category);
+    if (!category) {
+        return res.status(400).send(`Invalid Category`);
+    }
+
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            longDescription: req.body.longDescription,
+            images: req.body.images,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured,
+        }, {
+            new: true,
+        }
+    )
+
+    if (!product) {
+        res.status(500).json({success: false, message: 'Product not found!'});
+    }
+
+    res.send(product);
+})
+
 module.exports = router;
