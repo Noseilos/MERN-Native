4b6ec2eae36cfd0bab238ebbb964c99550ace727
Noseilos <jonaseguiron@gmail.com>
2023-12-26 00:08:09 +0800

    register user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,4 +12,27 @@ router.get(`/`, async (req, res) => {
     res.send(users);
 })
 
+router.post(`/`, async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country
+    })
+
+    user = await user.save();
+
+    if (!user){
+        return res.status(404).send('User was not created')
+    }
+
+    res.send(user);
+});
+
 module.exports = router;
