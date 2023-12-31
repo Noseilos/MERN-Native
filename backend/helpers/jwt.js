557c54e32bae62fa7beb62dc940fe32c4c46af33
Noseilos <jonaseguiron@gmail.com>
2023-12-31 11:23:31 +0800

    excluding rest apis

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -3,10 +3,18 @@ const { expressjwt } = require('express-jwt')
 function authJwt(){
     console.log('Executing authJwt middleware');
     const secret = process.env.JWT_SECRET;
+    const api = process.env.API_URL
 
     return expressjwt({
         secret,
         algorithms: ['HS256']
+    }).unless({
+        path: [
+            {url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
+            {url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS']},
+            `${api}/users/login`,
+            `${api}/users/register`
+        ]
     })
 }
 
