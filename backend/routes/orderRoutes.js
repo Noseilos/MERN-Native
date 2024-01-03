c02bfefc60466cf53a6fa0103860f620e2c0f28c
Noseilos <jonaseguiron@gmail.com>
2024-01-03 23:59:18 +0800

    new order backend

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Order = require('../models/Order');
+const OrderItem = require('../models/OrderItem');
 
 router.get(`/`, async (req, res) => {
     const orders = Order.find();
@@ -10,6 +11,40 @@ router.get(`/`, async (req, res) => {
     }
 
     res.send(orders);
-})
+});
+
+router.post(`/`, async (req, res) => {
+    const orderItemsIds = req.body.orderItems.map(async orderItem => {
+        let newOrderItem = new OrderItem({
+            quantity: orderItem.quantity,
+            product: orderItem.product
+        });
+
+        newOrderItem = await newOrderItem.save();
+
+        return newOrderItem._id
+    })
+
+    let order = new Order({
+        orderItems: orderItemsIds,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user,
+    })
+
+    order = await order.save();
+
+    if (!order){
+        return res.status(404).send('Order was not created')
+    }
+
+    res.send(order);
+});
 
 module.exports = router;
