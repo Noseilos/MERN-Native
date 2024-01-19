2cffca6eab465f4a33b00f757373400b86e49f8b
Noseilos <jonaseguiron@gmail.com>
2024-01-19 15:04:56 +0800

    screens and folders

---
+++ frontend/Screens/Products/ProductContainer.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
+
+
+const ProductContainter = () => {
+
+    return (
+        <View>
+            <Text>Product Container</Text>
+        </View>
+    )
+}
+
+export default ProductContainter;
