17244f808e6d34b3f100f7cca10a4a7dbe8322f6
Noseilos <jonaseguiron@gmail.com>
2024-01-19 15:13:12 +0800

    flatlisting products

--- frontend/Screens/Products/ProductContainer.js
+++ frontend/Screens/Products/ProductContainer.js
@@ -1,12 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
 
 
+const data = require('../../assets/data/products.json')
+
 const ProductContainter = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() =>{
+        setProducts(data)
+
+        return () => {
+            setProducts([]);
+        }
+    },[])
 
     return (
         <View>
+            <View style={{ marginTop: 100 }}>
             <Text>Product Container</Text>
+                <FlatList
+                    horizontal
+                    data={products}
+                    renderItem={({item}) => <Text>{item.brand}</Text>}
+                    keyExtractor={item => item.name}
+                />
+            </View>
         </View>
     )
 }
