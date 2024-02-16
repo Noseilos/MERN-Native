5be7d0b4ab677bbb19a689fd70e1164c79d8fa70
Noseilos <jonaseguiron@gmail.com>
2024-02-16 14:41:47 +0800

    native-base installed

--- frontend/Screens/Products/ProductContainer.js
+++ frontend/Screens/Products/ProductContainer.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
+import { View, StyleSheet, ActivityIndicator, FlatList, Dimensions } from 'react-native';
+import { Container, Header, Icon, Item, Input, Text } from 'native-base';
 
 import ProductList from './ProductList';
+
+var { height } = Dimensions.get('window')
 
 const data = require('../../assets/data/products.json')
 
@@ -19,29 +22,50 @@ const ProductContainter = () => {
     },[]);
 
     return (
-        <View style={styles.container}>
-            <FlatList
-                numColumns={numColumns}
-                key={numColumns}
-                showsHorizontalScrollIndicator={false}
-                data={products}
-                renderItem={({ item }) => <ProductList key={item.id} item={item} />}
-                keyExtractor={(item) => item.name}
-            />
-        </View>
+        <Container>
+            <Header searchBar rounded>
+                <Item>
+                    <Icon name='ios-search'/>
+                    <Input
+                        placeholder='Search'
+                        // onFocus = {}
+                        // onChangeText = {(test) =>}
+                    />
+                </Item>
+            </Header>
+            <View style={styles.container}>
+                <View style={styles.listContainer}>
+                    <FlatList
+                        numColumns={numColumns}
+                        key={numColumns}
+                        showsHorizontalScrollIndicator={false}
+                        data={products}
+                        renderItem={({ item }) => <ProductList key={item.id} item={item} />}
+                        keyExtractor={(item) => item.name}
+                    />
+                </View>
+            </View>
+        </Container>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-      marginTop: 50,
+      flexWrap: "wrap",
+      backgroundColor: "gainsboro",
     },
-    heading: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      marginBottom: 10,
+    listContainer: {
+      height: height,
+      flex: 1,
+      flexDirection: "row",
+      alignItems: "flex-start",
+      flexWrap: "wrap",
+      backgroundColor: "gainsboro",
     },
+    center: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
   });
 
 export default ProductContainter;
